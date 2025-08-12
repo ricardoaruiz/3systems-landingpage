9b80585aa8d9e8af3cb078300fc4598621d5a405
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-08-12 17:41:07 -0300

    feat: 🎸 create Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { Header } from '@/components/header';
+
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-center font-bold text-3xl">
-        Cristallo Empresarial - Landing Page
-      </h1>
-    </div>
+    <main>
+      <Header />
+    </main>
   );
 }
---
+++ src/components/header/header.view.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+
+export function HeaderView() {
+  return (
+    <header className="flex items-center justify-center bg-zinc-100 p-4 lg:p-8">
+      <div className="relative size-[140px] lg:size-[280px]">
+        <Image alt="Logo" fill src={logo} />
+      </div>
+    </header>
+  );
+}
