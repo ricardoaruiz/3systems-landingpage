fc8ed514ee4f3ceff37214ab8cb26fb6ca7abb55
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-08-22 13:56:57 -0300

    feat: 🎸 adding ping animation on whatsapp button

--- src/components/whatsapp-button/whatsapp-button.view.tsx
+++ src/components/whatsapp-button/whatsapp-button.view.tsx
@@ -14,22 +14,25 @@ export function WhatsAppButtonView({
     <div className="fixed right-4 bottom-20 z-50 lg:bottom-24">
       <Tooltip>
         <TooltipTrigger asChild>
-          <a
-            className={cn(
-              'flex size-12 animate-pulse cursor-pointer items-center justify-center rounded-full border border-zinc-500 bg-secondary-cristallo p-2 text-zinc-100 shadow-lg shadow-zinc-800 hover:bg-primary-cristallo md:size-16',
-              className
-            )}
-            rel="noopener noreferrer"
-            target="_blank"
-            {...props}
-          >
-            <Image
-              alt="WhatsApp icone"
-              height={48}
-              src={whatsappIcon}
-              width={48}
-            />
-          </a>
+          <div className="relative flex size-12 md:size-16">
+            <div className="absolute inline-flex h-full w-full animate-ping rounded-full bg-zinc-400 opacity-40" />
+            <a
+              className={cn(
+                'relative inline-flex size-12 cursor-pointer items-center justify-center rounded-full border border-zinc-500 bg-secondary-cristallo p-2 text-zinc-100 shadow-lg shadow-zinc-800 hover:bg-primary-cristallo md:size-16',
+                className
+              )}
+              rel="noopener noreferrer"
+              target="_blank"
+              {...props}
+            >
+              <Image
+                alt="WhatsApp icone"
+                height={48}
+                src={whatsappIcon}
+                width={48}
+              />
+            </a>
+          </div>
         </TooltipTrigger>
         <TooltipContent>
           <p>Fale conosco no WhatsApp</p>
