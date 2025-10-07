c97792b8ab948f4f794311b494c7f65143930a6b
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-10-07 15:10:13 -0300

    fix: 🐛 robots generator function

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { DOMAIN_URL } from "@/constants";
 
+const domain = DOMAIN_URL.endsWith("/") ? DOMAIN_URL.slice(0, -1) : DOMAIN_URL;
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
@@ -8,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: `${DOMAIN_URL}/sitemap.xml`,
+    sitemap: `${domain}/sitemap.xml`,
   };
 }
